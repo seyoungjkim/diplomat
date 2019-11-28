3a85229bf943b1938009f928e66c9dbaf551e48b
Weizhen Sheng <sheng.weizhen@gmail.com>
2019-11-28 02:08:59 +0100

    main Game.hs

--- Game.hs
+++ Game.hs
@@ -2,6 +2,7 @@
 module Game where
 import qualified State as S
 import qualified Data.Maybe as Maybe
+import Text.Read
 import Question
 import GameState
 
@@ -26,10 +27,27 @@ main numPlayers numAI = let initialStore = initialGameStore numPlayers numAI
                             sequence = players initialStore in
   go sequence initialStore where
     go :: [Player] -> GameStore -> IO()
-    go sequence store = let (sequence', store') = S.runState (move sequence) store in do
-        putStrLn (show (sequence !! 0))
-        str <- getLine
-        go sequence' store'
+    go sequence store = 
+      let (sequence', store') = S.runState (move sequence) store in do
+        let player = sequence !! 0
+        let pids = foldr (\p acc -> (pid p) : acc) [] (players store)
+        putStrLn ("Your hand:")
+        putStrLn (show $ hand player)
+        putStr ("Player " ++ (show $ pid player) ++ ": Enter a player id> ")
+        playerToQuestion <- getLine
+        case (readMaybe playerToQuestion :: Maybe Int) of
+          Just i -> if elem i pids then do
+                      putStr ("Player " ++ (show $ pid player) ++ ": Enter a question> ")
+                      q <- getLine
+                      case q of
+                        "q" -> return () -- quit the game
+                        "none" -> go sequence' store' -- skip turn
+                        _   -> putStrLn "invalid question" >> go sequence store -- unknown command
+                    else putStrLn "please enter a valid player id" >> go sequence store
+          Nothing -> putStrLn "please enter a player id" >> go sequence store
+        
+          
+        
 
 move :: [Player] -> Game
 move [] = return []
